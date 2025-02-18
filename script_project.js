968c9acec075efc13b4a5175612bacb04e378395
SushrutaBera <sushurutabera1502@gmail.com>
2025-02-18 14:43:52 +0530

    Final Commit

---
+++ script_project.js
@@ -0,0 +1,8 @@
+function toggleCollapse(button) {
+    let content = button.nextElementSibling;
+    if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "px";
+    }
+}
